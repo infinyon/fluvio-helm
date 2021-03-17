d23be7838c8105b4bb4d30f35f4d2410088b3af1
mibes <mibes@avaya.com>
2021-03-17 10:16:59 +0100

    Return additional attributes in InstalledChart

--- src/lib.rs
+++ src/lib.rs
@@ -398,4 +398,30 @@ pub struct InstalledChart {
     pub name: String,
     /// The version of the app this chart installed
     pub app_version: String,
+    /// The chart revision
+    pub revision: String,
+    /// Date/time when the chart was last updated
+    pub updated: String,
+    /// Status of the installed chart
+    pub status: String,
+    /// The ID of the chart that is installed
+    pub chart: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_get_installed_charts() {
+        const JSON_RESPONSE: &str = r#"[{"name":"test_chart","namespace":"default","revision":"50","updated":"2021-03-17 08:42:54.546347741 +0000 UTC","status":"deployed","chart":"test_chart-1.2.32-rc2","app_version":"1.2.32-rc2"}]"#;
+        let installed_charts: Vec<InstalledChart> =
+            serde_json::from_slice(JSON_RESPONSE.as_bytes()).expect("can not parse json");
+        assert_eq!(installed_charts.len(), 1);
+        let test_chart = installed_charts
+            .get(0)
+            .expect("can not grab the first result");
+        assert_eq!(test_chart.name, "test_chart");
+        assert_eq!(test_chart.chart, "test_chart-1.2.32-rc2");
+    }
 }
